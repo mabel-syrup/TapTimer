769d09b2cc59bb4e8886415decea672033f50ece
mjSurber <matthewsurber96@gmail.com>
2016-09-26 23:49:48 -0500

    Initial commit.

---
+++ app/src/main/java/com/surber/matthew/taptimer/MainActivity.java
@@ -0,0 +1,35 @@
+package com.surber.matthew.taptimer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        final ArrayList<String> times = new ArrayList<>();
+        final SimpleDateFormat timeFormat = new SimpleDateFormat("HH:mm:ss");
+        Button timeButton = (Button) findViewById(R.id.time_button);
+        final ArrayAdapter<String> timeListAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1, times);
+        final ListView timeList = (ListView) findViewById(R.id.time_list);
+        timeList.setAdapter(timeListAdapter);
+
+        timeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                times.add(timeFormat.format(new Date()));
+                timeListAdapter.notifyDataSetChanged();
+            }
+        });
+    }
+}
